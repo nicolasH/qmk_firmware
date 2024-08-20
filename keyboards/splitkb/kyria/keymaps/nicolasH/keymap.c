240152f39e1c392fe549ddeb369e63a6a7f28924
Nicolas Hoibian <nicolas.hoibian@wipo.int>
2024-08-20 21:08:45 +0200

    Merged changes from datcyl keymap, swapped thumb keys around

--- keyboards/splitkb/kyria/keymaps/nicolasH/keymap.c
+++ keyboards/splitkb/kyria/keymaps/nicolasH/keymap.c
@@ -47,6 +47,16 @@ enum layers {
 #define LT_NAV_BSP LT(_NAV,KC_BSPC)
 
 
+#define LT_N_S_SPC LT(_NUM_SYM,KC_SPC)
+#define LT_F_N_TAB LT(_FUN_NAV,KC_TAB)
+
+#define LT_N_S_ENT LT(_NUM_SYM,KC_ENT)
+#define LT_F_N_BSP LT(_FUN_NAV,KC_BSPC)
+
+
+#define LT_RGB_ESC LT(_RGB,KC_BSPC)
+#define LT_RGB_DEL LT(_RGB,KC_DEL)
+
 #define ____ KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -56,12 +66,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                              ,-------------------------------------------.
  * |    ~   |   Q  |   W  |   F  |   P  |   B  |                              |   J  |   L  |   U  |   Y  |   ;  |  | \   |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |Ctrl/Tab|   A  |   R  |  S   |   T  |   G  |                              |   M  |   N  |   E  |   I  |  O   |  ' "   |
+ * |Ctrl/Tab|   A  |   R  |   S  |   T  |   G  |                              |   M  |   N  |   E  |   I  |  O   |  ' "   |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |    `   |   Z  |   X  |   C  |   V  |   B  | ---- | ---- |  | ---- | ---- |   K  |   H  | ,  < | . >  | /  ? |  - _   |
+ * |    `   |   Z  |   X  |   C  |   D  |   V  | ---- | ---- |  | ---- | ---- |   K  |   H  | ,  < | . >  | /  ? |  - _   |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |  "   |   '  | Esc  | Space| Tab  |  | Del  | Bksp    | Ret  l      |      |
- *                        |      |      |      | Num  | Fn   |  | Sym  | Nav  |      |      |      |
+ *                        |  "   |   '  | Space| Tab  | Esc  |  | Del  | Ent  | Bksp |      |      |
+ *                        |      |      |Num Sy| F Nav| RGB  |  |      |Num Sy| F Nav|      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
 
@@ -69,37 +79,56 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TILDE,       KC_Q,   KC_W,   KC_F,   KC_P,   KC_B,                                                          KC_J,     KC_L,    KC_U,    KC_Y,    KC_SCLN,  KC_PIPE,
       LCTL_T(KC_TAB), MC_A,   MA_R,   MS_S,   MG_T,   KC_G,                                                          KC_M,     MG_N,    MS_E,    MA_I,    MC_O,     KC_BSLASH,
       KC_GRV,         KC_Z,   KC_X,   KC_C,   KC_D,   KC_V, _______, _______,                      _______, _______, KC_K,     KC_H,    KC_COMM, KC_DOT,  KC_SLSH,  KC_EQUAL,
-                                KC_DQT, KC_QUOT,  KC_ESC,   LT_NUM_SPC,  LT_FUN_TAB,                        LT_SYM_ENT,  LT_NAV_BSP, KC_DEL,     KC_MINS, KC_UNDS
+                                KC_DQT, KC_QUOT, LT_N_S_SPC, LT_F_N_TAB, KC_ESC,                  KC_DEL , LT_N_S_ENT, LT_F_N_BSP,    KC_MINS, KC_UNDS
 						       ),
 
 
-
-      [_SYM] = LAYOUT(
-		_______,_______, _______, _______, _______, _______,                                    KC_EXLM, KC_AT ,  KC_LCBR, KC_RCBR, KC_AMPR,_______,
-		_______,RGB_TOG, RGB_SAI, RGB_HUI, RGB_VAI, RGB_MOD,                                    KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_ASTR,_______,
-		_______,_______, RGB_SAD, RGB_HUD, RGB_VAD, RGB_RMOD,_______,_______,  _______,_______, KC_PERC, KC_CIRC, KC_LT,   KC_GT,   KC_DQUO,_______,
-		                      _______,_______,_______,_______,_______,              _______,_______,_______, KC_LBRC, KC_RBRC
+  [_RGB] = LAYOUT(
+		_______,_______, _______, _______, _______, _______,                                    _______,_______,_______,_______,_______,_______,
+		_______,RGB_TOG, RGB_SAI, RGB_HUI, RGB_VAI, RGB_MOD,                                    _______,_______,_______,_______,_______,_______,
+		_______,_______, RGB_SAD, RGB_HUD, RGB_VAD, RGB_RMOD,_______,_______,  _______,_______, _______,_______,_______,_______,_______,_______,
+		                      _______,_______,_______,_______,_______,               _______,_______,_______,_______,_______,
 		  ),
+  [_NUM_SYM] = LAYOUT(
+		  _______,KC_PLUS , KC_7,    KC_8,    KC_9,  KC_ASTR,                                   KC_EXLM, KC_AT ,  KC_LCBR, KC_RCBR, KC_AMPR,_______,
+		  _______,KC_MINS , KC_4,    KC_5,    KC_6,  KC_SLASH,                                  KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_ASTR,_______,
+		  _______,KC_EQUAL, KC_1,    KC_2,    KC_3,  KC_ENTER,_______,_______,  _______,_______,KC_PERC, KC_CIRC, KC_LT,   KC_GT,   KC_DQUO,_______,
+                                     KC_0,   KC_DOT,  _______,        _______,_______,  _______,_______,         _______, KC_LBRC, KC_RBRC
+		  ),
+  [_FUN_NAV] = LAYOUT(
+		  _______,_______, KC_F7,    KC_F8,   KC_F9, KC_F10,                                    _______,_______,KC_PGUP,_______,_______, KC_INSERT,
+		  _______,_______, KC_F4,    KC_F5,   KC_F6, KC_F11,                                    KC_HOME,KC_LEFT,KC_UP  ,KC_DOWN,KC_RIGHT,KC_END ,
+		  _______,KC_EJCT, KC_F1,    KC_F2,   KC_F3, KC_F12,_______,_______,    _______,_______,_______,_______,_______,KC_PGDN,_______,_______,
+	                                    _______,_______,_______,_______,_______,    _______,_______,_______,_______,_______
+		      ),
+
+  ///////////////
+  ///////////////
+  
   [_NAV] = LAYOUT(
 		_______,_______,_______,_______,_______,_______,                                   _______,_______,KC_PGUP,_______ ,_______, KC_INSERT,
 		_______,_______,_______,_______,_______,_______,                                   KC_HOME,KC_LEFT,KC_UP  ,KC_DOWN, KC_RIGHT,KC_END ,
 		_______,_______,_______,_______,_______,_______,_______,_______,  _______,_______, _______, _______,_______,KC_PGDN,_______,_______,
 		                        _______,_______,_______,_______,_______,  _______,_______,_______,_______,_______
 		  ),
-  [_NUM] = LAYOUT(
-		  _______,KC_PLUS , KC_7,    KC_8,    KC_9,  KC_ASTR,                                   _______,_______,_______,_______,_______,_______,
-		  _______,KC_MINS , KC_4,    KC_5,    KC_6,  KC_SLASH,                                  _______,_______,_______,_______,_______,_______,
-		  _______,KC_EQUAL, KC_1,    KC_2,    KC_3,  KC_ENTER,_______,_______,  _______,_______,_______,_______,_______,_______,_______,_______,
-                                     KC_0,   KC_DOT,  _______,        _______,_______,  _______,_______,_______,_______,_______
-		  ),
+
   [_FUN] = LAYOUT(
 		  _______,_______, KC_F7,    KC_F8,   KC_F9, KC_F10,                                    _______,_______,_______,_______,_______,_______,
 		  _______,_______, KC_F4,    KC_F5,   KC_F6, KC_F11,                                    _______,_______,_______,_______,_______,_______,
 		  _______,KC_EJCT, KC_F1,    KC_F2,   KC_F3, KC_F12,_______,_______,    _______,_______,_______,_______,_______,_______,_______,_______,
 	                                    _______,_______,_______,_______,_______,    _______,_______,_______,_______,_______
+		  ),
+
+
+  
+  [_SYM] = LAYOUT(
+		_______,_______, _______, _______, _______, _______,                                    KC_EXLM, KC_AT ,  KC_LCBR, KC_RCBR, KC_AMPR,_______,
+		_______,RGB_TOG, RGB_SAI, RGB_HUI, RGB_VAI, RGB_MOD,                                    KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_ASTR,_______,
+		_______,_______, RGB_SAD, RGB_HUD, RGB_VAD, RGB_RMOD,_______,_______,  _______,_______, KC_PERC, KC_CIRC, KC_LT,   KC_GT,   KC_DQUO,_______,
+		                      _______,_______,_______,_______,_______,              _______,_______,_______, KC_LBRC, KC_RBRC
 		  )
 
-      
+  
 // /*
 //  * Layer template
 //  *
@@ -151,15 +180,15 @@ static void render_status_keymap(void) {
       oled_write_P(PSTR("  ~  Q W F P B     \n"), false);
       oled_write_P(PSTR(" TAB A R S T G     \n"), false);
       oled_write_P(PSTR("  `  Z X C D V     \n"), false);
-      oled_write_P(PSTR("     \" ' ESC SPC TAB\n"), false);
+      oled_write_P(PSTR("     \" ' SPC TAB ESC\n"), false);
     }else{
       oled_write_P(PSTR("         J L U Y ; |\n"), false);
       oled_write_P(PSTR("         M N E I O \\\n"), false);
       oled_write_P(PSTR("         K H , . / =\n"), false);
-      oled_write_P(PSTR(" ENT BSP DEL - _   \n"), false);      
+      oled_write_P(PSTR(" DEL ENT BSP - _   \n"), false);      
     }
        break;
-  case _SYM:
+  case _RGB:
     //                   __________|__________
     if (is_keyboard_master()){
       /* _______,_______,_______,_______,_______,_______,         */
@@ -172,21 +201,21 @@ static void render_status_keymap(void) {
       oled_write_P(PSTR("           - - - - -\n"), false);
     }
     else { //            __________|__________
-      oled_write_P(PSTR("       ! @ [ ] &  --\n"),false);
-      oled_write_P(PSTR("       # $ ( ) *  --\n"),false);
-      oled_write_P(PSTR("       % ^ < > \"  --\n"),false);
-      oled_write_P(PSTR(" -- -- --  { } \n"),false);
+      oled_write_P(PSTR("\n"), false);
+      oled_write_P(PSTR("\n"), false);
+      oled_write_P(PSTR("\n"), false);
+      oled_write_P(PSTR("\n"), false);
     }
 
 
     break;
-  case _NAV:
+  case _FUN_NAV:
     if (is_keyboard_master()){
       //                 __________|__________
-      oled_write_P(PSTR("\n"), false);
-      oled_write_P(PSTR("\n"), false);
-      oled_write_P(PSTR("\n"), false);
-      oled_write_P(PSTR("\n"), false);
+      oled_write_P(PSTR("-- -- F7 F8 F9 F10 \n"), false);
+      oled_write_P(PSTR("-- -- F4 F5 F6 F11 \n"), false);
+      oled_write_P(PSTR("-- -- F1 F2 F3 F12 \n"), false);
+      oled_write_P(PSTR("      -- -- -- -- --\n"), false);	  
     }else{
       //                 __________|__________
       oled_write_P(PSTR("    - - PGUP - - Ins\n"), false);
@@ -195,7 +224,7 @@ static void render_status_keymap(void) {
       oled_write_P(PSTR("- - - - - \n"), false);
     }
     break;
-  case _NUM:
+  case _NUM_SYM:
     if (is_keyboard_master()){
       //                 __________|__________
       oled_write_P(PSTR(" --  + 7 8 9 *     \n"), false);
@@ -204,28 +233,28 @@ static void render_status_keymap(void) {
       oled_write_P(PSTR("        0 . -- -- --\n"), false);	  
     }else{
       //                 __________|__________
-      oled_write_P(PSTR("\n"), false);
-      oled_write_P(PSTR("\n"), false);
-      oled_write_P(PSTR("\n"), false);
-      oled_write_P(PSTR("\n"), false);
+      oled_write_P(PSTR("       ! @ [ ] &  --\n"),false);
+      oled_write_P(PSTR("       # $ ( ) *  --\n"),false);
+      oled_write_P(PSTR("       % ^ < > \"  --\n"),false);
+      oled_write_P(PSTR(" -- -- --  { } \n"),false);
     }
     break;
-  case _FUN:
-    if (is_keyboard_master()){    
-      //                 __________|__________
-      oled_write_P(PSTR("-- -- F7 F8 F9 F10 \n"), false);
-      oled_write_P(PSTR("-- -- F4 F5 F6 F11 \n"), false);
-      oled_write_P(PSTR("-- -- F1 F2 F3 F12 \n"), false);
-      oled_write_P(PSTR("      -- -- -- -- --\n"), false);	  
+  /* case _FUN: */
+  /*   if (is_keyboard_master()){     */
+  /*     //                 __________|__________ */
+  /*     oled_write_P(PSTR("-- -- F7 F8 F9 F10 \n"), false); */
+  /*     oled_write_P(PSTR("-- -- F4 F5 F6 F11 \n"), false); */
+  /*     oled_write_P(PSTR("-- -- F1 F2 F3 F12 \n"), false); */
+  /*     oled_write_P(PSTR("      -- -- -- -- --\n"), false);	   */
 
-    } else{
-      //                 __________|__________
-      oled_write_P(PSTR("\n"), false);
-      oled_write_P(PSTR("\n"), false);
-      oled_write_P(PSTR("\n"), false);
-      oled_write_P(PSTR("\n"), false);
-    }
-    break;
+  /*   } else{ */
+  /*     //                 __________|__________ */
+  /*     oled_write_P(PSTR("\n"), false); */
+  /*     oled_write_P(PSTR("\n"), false); */
+  /*     oled_write_P(PSTR("\n"), false); */
+  /*     oled_write_P(PSTR("\n"), false); */
+  /*   } */
+  /*   break; */
   default:
     //                 __________|__________
     oled_write_P(PSTR(" ---- Undefined ---- \n"), false);    
