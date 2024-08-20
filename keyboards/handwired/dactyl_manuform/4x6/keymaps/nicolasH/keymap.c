8192cb7224120a1a36c5c98f511eb47b5288dd19
Nicolas Hoibian <nicolas.hoibian@wipo.int>
2024-08-20 21:10:04 +0200

    Changed spacing for aesthetic reasons

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/nicolasH/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/nicolasH/keymap.c
@@ -113,8 +113,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 						),
 [_NAV] = LAYOUT(
 		_______,KC_BRIU,KC_F7    ,KC_F8    ,KC_F9  ,KC_F10,     _______,_______,KC_PGUP,_______ ,_______ ,KC_INSERT,
-	        _______,KC_BRID,KC_F4    ,KC_F5    ,KC_F6  ,KC_F11,  KC_HOME,KC_LEFT,KC_UP  ,KC_DOWN ,KC_RIGHT,KC_END ,
-		_______,KC_EJCT,KC_F1    ,KC_F2    ,KC_F3  ,KC_F12, _______,_______,_______,KC_PGDN, _______ ,_______,
+	        _______,KC_BRID,KC_F4    ,KC_F5    ,KC_F6  ,KC_F11,     KC_HOME,KC_LEFT,KC_UP  ,KC_DOWN ,KC_RIGHT,KC_END ,
+		_______,KC_EJCT,KC_F1    ,KC_F2    ,KC_F3  ,KC_F12,     _______,_______,_______,KC_PGDN, _______ ,_______,
 		                KC_VOLD , KC_VOLU,                                   _______,_______,
 		                                _______,_______,    _______,_______,
 		                                _______,_______,    _______,_______,
